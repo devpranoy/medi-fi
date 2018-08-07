a7614c9d332981f863b57dbfcee58f16a06a94fd
Pranoy Dev <devpranoy@gmail.com>
2018-08-07 14:18:50 +0530

    removed wtfroms

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask ,render_template, flash, redirect, url_for, session, request, logging
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
 import dbquery
