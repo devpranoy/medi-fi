07eb9b6da28acdf9bed50bcd6692cff0e86fb5ea
Pranoy Dev <devpranoy@gmail.com>
2018-08-07 14:58:54 +0530

    huge changes

--- app.py
+++ app.py
@@ -1,54 +1,69 @@
 from flask import Flask ,render_template, flash, redirect, url_for, session, request, logging
+import psycopg2
+from functools import wraps
 import dbquery
+import json
 
 app = Flask(__name__)
+def is_logged_in(f):	# Function for implementing security and redirection
+	@wraps(f)
+	def wrap(*args,**kwargs):
+		if 'logged_in' in session:
+			return f(*args, **kwargs)
+		else:
+			flash('Unauthorised, Please Login')
+			return redirect(url_for('login'))
+	return wrap	# A wrap is a concept that is used to check for authorisation of a request
 
-@app.route('/dashboard',methods=['GET','POST'])
-def dashboard():
-	sql="SELECT * FROM TOILETS WHERE USERID = '%s'"%(session['userid'])
-	people=dbquery.fetchall(sql)
-	return render_template('dashboard.html',people=people)
 
 
 @app.route('/login', methods=['GET','POST']) #login page
 def login():
 	if request.method == 'POST':
-		email = request.form['email']					#GET FORM FIELDS
+		error = None
+		username = request.form['username']					#GET FORM FIELDS
 		password_candidate= request.form['password']	#GET FORM FIELDS
 		flag=0
-		
-
-		sql="SELECT PASSWORD FROM USERS WHERE EMAIL= '%s' "%(email)
+		sql="SELECT password FROM users WHERE username = '%s' "%(username)
 		rows = dbquery.fetchone(sql)
 		try:				# if no entry found, an error is raised
 			for row in rows:
 				flag=1
 				password=row
-			sql="SELECT NAME FROM USERS WHERE EMAIL= '%s' "%(email)		#validations
-			rows = dbquery.fetchone(sql)
-			for row in rows:
-				name=row
-			sql="SELECT USERID FROM USERS WHERE EMAIL= '%s' "%(email)	#validations
+			sql="SELECT  id FROM users WHERE username = '%s' "%(username)		#validations
 			rows = dbquery.fetchone(sql)
 			for row in rows:
 				userid=row
-			
+			sql="SELECT type FROM users WHERE username= '%s' "%(username)	#validations
+			rows = dbquery.fetchone(sql)
 			for row in rows:
-				city=row
+				type=row
+			
 			if str(password_candidate) == str(password):	#initialise session variable if passwords match
 				session['logged_in'] = True
-				session['name'] = str(name)
-				session['userid']=userid
+				session['username'] = str(username)
+				session['type']=str(type)
+				session['id']=str(userid)
 			
                 
 			else:
-				error = 'Invalid login'
+				error = 'Username or Password Incorrect'
 				return render_template('login.html',error=error)
 		except:
 			if flag==0:
-				error = 'Email not found'
+				error = 'Username or Password Incorrect'
 				return render_template('login.html',error=error)
-		return redirect(url_for('dashboard'))#if verification is successful load the dashboard with session
+
+
+		data = {
+		"status": "Success",
+        "userID": str(userid),
+        "type": str(type),
+		"userName": str(username)
+    	}
+		json_string = json.dumps(data)
+		return json_string
+	#if verification is successful load the dashboard with session
 	return render_template('login.html')
 @app.route('/logout')
 def logout():
@@ -59,21 +74,22 @@ def logout():
 @app.route('/signup', methods=['GET','POST'])
 def signup():
 	if request.method == 'POST':
-		email = request.form['email']					#GET FORM FIELDS
-		password_candidate= request.form['password']
-		name = request.form['name']
-		sql="SELECT USERID FROM USERS WHERE EMAIL='%s'"%(email) #Security check on email
+		message=None
+		username = request.form['username']					#GET FORM FIELDS
+		password= request.form['password']
+		type = request.form['type']
+		sql="SELECT id FROM users WHERE username='%s'"%(username) #Security check on username
 		try:
 			rows = dbquery.fetchone(sql) #if none, error should be raised
 			for row in rows:
 				f=1
 		except:
-			sql="INSERT INTO USERS(NAME,EMAIL,PASSWORD) VALUES('%s','%s' ,'%s')"%(name,email,password_candidate)
+			sql="INSERT INTO users(username,password,type) VALUES('%s','%s' ,'%s')"%(username,password,type)
 			dbquery.inserttodb(sql)	#connecting to db model
-			flash('You are now registered! Please Log in.','success') #sending a message to user
-			return redirect(url_for('login')) #redirecting to login page
-		flash('This Email exists!','success') #Checking for email
-		return render_template('signup.html')
+			message="User Registration Successful"
+			return render_template('signup.html',message=message)
+		message="Username Exists"
+		return render_template('signup.html',message=message)
 	return render_template('signup.html')
 
 @app.route('/', methods=['GET','POST'])
@@ -82,4 +98,4 @@ def index():
 
 if __name__=='__main__':
 	app.secret_key='secret123' #for flash messaging
-	app.run(host='0.0.0.0',port =80,threaded=True) #Debugger is set to 1 for testing and overriding the default port to http port
+	app.run(threaded=True,host="localhost",port=80) #Debugger is set to 1 for testing and overriding the default port to http port
